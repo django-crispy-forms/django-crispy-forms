c1d53c5a05244ecfc585794d19893c9e8b773350
Daniel Greenfeld <pydanny@gmail.com>
2009-01-12 17:47:35 +0000

    Incorporating more the rich goodness of uni_form
    git-svn-id: http://django-uni-form.googlecode.com/svn/trunk@3 cf8eee90-dced-11dd-bad4-6dbcaf1fdc36

--- uni_form/templatetags/uni_form.py
+++ uni_form/templatetags/uni_form.py
@@ -1,23 +1,15 @@
-from django import template
-register = template.Library()
+from django.template import Context, Template
+from django.template.loader import get_template
 from django.utils.safestring import mark_safe
+from django import template
 
 
+register = template.Library()
 
 @register.filter
 def as_uni_form(form):
-    # TODO:
-    #   1. Use django template to render the text
-    #   2. possible escape the text in the rendered fields
-    
-    text = ''
-    for field in form:
-        text += """
-            <div class="ctrlHolder">
-                %s
-                %s : %s
-            </div>
-        """ % (field.errors, field.label_tag(), field)
-    return mark_safe(text)
-    
+    template = get_template('templates/uni_form.html')
+    c = Context({'form':form})
 
+    return template.render(c)
+    
