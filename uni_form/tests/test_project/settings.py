438b77bb7b424edc991863436ed0f7f74dcde554
Daniel Greenfeld <pydanny@gmail.com>
2009-07-18 21:08:32 -0400

    Finally getting the test page working the way I want. Not using static media because I want to see all the rendered elements on the page in a nice and easy to debug way.

--- uni_form/tests/test_project/settings.py
+++ uni_form/tests/test_project/settings.py
@@ -42,7 +42,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = join(PROJECT_ROOT, 'media')
+MEDIA_ROOT = ""
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -76,6 +76,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "uni_form", "media", "uni_form"),    
 )
 
 INSTALLED_APPS = (
