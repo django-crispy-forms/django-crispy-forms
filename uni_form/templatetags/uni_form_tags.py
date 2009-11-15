5d9c3f4fa18a475f7a63afb352874b86943f7cd2
Daniel Greenfeld <pydanny@gmail.com>
2009-11-15 13:40:48 -0500

    Getting the test app to work right

--- uni_form/templatetags/uni_form_tags.py
+++ uni_form/templatetags/uni_form_tags.py
@@ -40,7 +40,7 @@ Create the <style> and <script> tags needed to initialize the uni-form.
 Create a local uni-form/includes.html template if you want to customize how
 these files are loaded. 
 """
-    return {'MEDIA_URL': context['MEDIA_URL']}
+    return {}
 
 ############################################################################
 #
--- uni_form/tests/test_project/settings.py
+++ uni_form/tests/test_project/settings.py
@@ -47,7 +47,7 @@ MEDIA_ROOT = ""
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
