5a8d9c854ab52e1b87868f3364b10983c29d6806
Daniel Greenfeld <pydanny@gmail.com>
2009-11-28 16:42:46 -0500

    Getting test pages to fire correctly

--- test_project/settings.py
+++ test_project/settings.py
@@ -42,7 +42,8 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ""
+MEDIA_ROOT = PROJECT_ROOT.replace('/test_project','/uni_form/media/')
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
