a86e9b2e6dfc7dfbdaa74ddebf19541f5800c57f
pydanny <pydanny@gmail.com>
2010-04-12 11:52:55 -0700

    Incorporating UniForm v1.4 into django-uni-form. Also improving test pages to more closely match Dragan's work so I can better judge the results

--- uni_form/media/uni_form/uni-form.jquery.js
+++ uni_form/media/uni_form/uni-form.jquery.js
@@ -1,3 +1,5 @@
+// Author: Ilija Studen for the purposes of Uni–Form
+
 jQuery.fn.uniform = function(settings) {
   settings = jQuery.extend({
     valid_class    : 'valid',
@@ -9,11 +11,26 @@ jQuery.fn.uniform = function(settings) {
   
   return this.each(function() {
     var form = jQuery(this);
- 
+    
+    // Focus specific control holder
+    var focusControlHolder = function(element) {
+      var parent = element.parent();
+      
+      while(typeof(parent) == 'object') {
+        if(parent) {
+          if(parent[0] && (parent[0].className.indexOf(settings.holder_class) >= 0)) {
+            parent.addClass(settings.focused_class);
+            return;
+          } // if
+        } // if
+        parent = jQuery(parent.parent());
+      } // while
+    };
+    
     // Select form fields and attach them higlighter functionality
     form.find(settings.field_selector).focus(function() {
       form.find('.' + settings.focused_class).removeClass(settings.focused_class);
-      jQuery(this).parents("." + settings.holder_class).addClass(settings.focused_class)
+      focusControlHolder(jQuery(this));
     }).blur(function() {
       form.find('.' + settings.focused_class).removeClass(settings.focused_class);
     });
@@ -22,9 +39,5 @@ jQuery.fn.uniform = function(settings) {
 
 // Auto set on page load...
 $(document).ready(function() {
-  q = jQuery('form.uniForm');
-  if(q.length) {
-    q.uniform();
-  }
-  $(document.activeElement).focus() //safari doesn't support this and has no alternative
+  jQuery('form.uniForm').uniform();
 });
