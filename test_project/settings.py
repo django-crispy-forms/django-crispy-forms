3d642327208ed445d0bb5cbbd7e49b9ee7a502bc
pydanny <pydanny@gmail.com>
2010-04-12 12:12:20 -0700

    Getting the test app to look better

--- test_project/settings.py
+++ test_project/settings.py
@@ -42,8 +42,8 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = PROJECT_ROOT.replace('/test_project','/uni_form/media/')
-
+MEDIA_ROOT = PROJECT_ROOT.replace('/test_project','/uni_form/media/uni_form')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
