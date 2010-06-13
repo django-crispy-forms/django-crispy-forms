aa571b2e1fd177491895cc263b192467431b90c2
Reavis Sutphin-Gray <reavis@sutphin-gray.com>
2010-06-13 09:46:52 +0800

    Made BaseInput inherit from forms.Field so inputs can be used in layouts. Added a SubmitButtonWidget.

--- uni_form/util.py
+++ uni_form/util.py
@@ -1,12 +1,37 @@
-class BaseInput(object):
+from django import forms
+from django.forms.widgets import Input
+
+class SubmitButtonWidget(Input):
+    """
+    A widget that handles a submit button.
+    """
+    input_type = 'submit'
+
+    def render(self, name, value, attrs=None):
+        return super(SubmitButtonWidget, self).render(name,
+            self.attrs['value'], attrs)
+        
+        
+class BaseInput(forms.Field):
     """
         An base Input class to reduce the amount of code in the Input classes.
     """
-    
-    def __init__(self,name,value):
-        self.name = name
-        self.value = value
-        
+    widget = SubmitButtonWidget
+
+    def __init__(self, **kwargs):
+        if not 'label' in kwargs:
+            kwargs['label'] = ''
+        if not 'required' in kwargs:
+            kwargs['required'] = False
+        if 'value' in kwargs:
+            self._widget_attrs = {'value': kwargs['value']}
+            del kwargs['value']
+        else:
+            self._widget_attrs = {'value': 'Submit'}
+        super(BaseInput, self).__init__(**kwargs)
+
+    def widget_attrs(self, widget):
+        return self._widget_attrs
 
 class Toggle(object):
     """
