6e8ad6c39bb8200aca873a0ccffe9a5e5e665562
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-03-16 12:34:38 +0100

    Removing util.py as BaseInput has been moved to helpers.
    Having an extra file for holding a 4 line class doesn't make sense anymore.

--- uni_form/util.py
+++
@@ -1,9 +0,0 @@
-class BaseInput(object):
-    """
-    A base Input class to reduce the amount of code in the Input classes.
-    """
-    
-    def __init__(self,name,value):
-        self.name = name
-        self.value = value
-        
