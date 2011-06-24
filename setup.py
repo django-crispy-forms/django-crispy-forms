56ff29b65d00fdf665f77901c202bd637731b7a0
pydanny <pydanny@gmail.com>
2011-06-24 13:57:31 -0700

    more tweaks for 0.8.0-beta release

--- setup.py
+++ setup.py
@@ -4,17 +4,33 @@ version = '0.8.0-beta'
  
 LONG_DESCRIPTION = """
 =====================================
-django-uni-form (Django Uni-Form)
+django-uni-form (django-uni-form)
 =====================================
 
-Django (http://djangoproject.com ) forms are easily rendered as tables,
+Django_ forms are easily rendered as tables,
 paragraphs, and unordered lists. However, elegantly rendered div based forms
 is something you have to do by hand. The purpose of this application is to
 provide a simple tag and/or filter that lets you quickly render forms in a div
 format.
 
-Uni-form (http://sprawsm.com/uni-form) has been selected as css/js library for
-the design of the forms.
+`Uni-form`_ has been selected as the base model for the design of the forms.
+
+Documentation
+=============
+
+http://readthedocs.org/docs/django-uni-form/en/latest/
+
+Contributions
+=============
+
+The maintainers of django-uni-form are committed to providing a fully tested
+suite of functionality for developers to use. Therefore, all submissions must
+come with either unit tests or browser views of functionality as is 
+demonstrated in the test projects. Inclusion of both will increase the likely
+hood of your pull request being accepted
+
+.. _`Uni-form`: http://sprawsm.com/uni-form
+.. _Django: http://djangoproject.com
 """
  
 setup(
