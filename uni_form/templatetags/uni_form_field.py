9ac0b90c6db21081db8f74f507fa251866b81f46
Jay McEntire <jay.mcentire@gmail.com>
2011-09-12 09:15:47 -0600

    Changed with_class function to use the built in css_classes function so that the return can still be 'field'.  By doing so this allows show_hidden_initial to not break.

--- uni_form/templatetags/uni_form_field.py
+++ uni_form/templatetags/uni_form_field.py
@@ -16,14 +16,9 @@ def is_checkbox(field):
 def with_class(field):
     class_name = field.field.widget.__class__.__name__.lower()
     class_name = class_converter.get(class_name, class_name)
-    if "class" in field.field.widget.attrs:
-        css_class = field.field.widget.attrs['class']
-        if field.field.widget.attrs['class'].find(class_name) == -1:
-            css_class += " %s" % (class_name,)
-    else:
-        css_class = class_name
+    field.field.widget.attrs['class'] = field.css_classes(extra_classes=class_name)
 
-    return field.as_widget(attrs={'class': css_class})    
+    return field  
 
 
 
