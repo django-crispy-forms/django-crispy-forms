b0fe522bf66ddac8227b954578368448e50b321b
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-10-25 20:34:27 +0200

    |with_class filter removes css classes, solves #GH-111

--- uni_form/templatetags/uni_form_field.py
+++ uni_form/templatetags/uni_form_field.py
@@ -16,6 +16,9 @@ def is_checkbox(field):
 def with_class(field):
     class_name = field.field.widget.__class__.__name__.lower()
     class_name = class_converter.get(class_name, class_name)
-    field.field.widget.attrs['class'] = field.css_classes(extra_classes=class_name)
+    css_class = field.field.widget.attrs.get('class', '')
+    if css_class.find(class_name) == -1:
+        css_class += " %s" % class_name
+    field.field.widget.attrs['class'] = css_class
 
     return field
