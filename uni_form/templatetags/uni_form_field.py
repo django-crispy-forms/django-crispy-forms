2aaee07daf8ee2186ac08567545474792ba4970c
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-10-25 22:01:38 +0200

    Fixing classes names consistency
    Bug introduced in version 0.9.0

--- uni_form/templatetags/uni_form_field.py
+++ uni_form/templatetags/uni_form_field.py
@@ -16,9 +16,13 @@ def is_checkbox(field):
 def with_class(field):
     class_name = field.field.widget.__class__.__name__.lower()
     class_name = class_converter.get(class_name, class_name)
+    
     css_class = field.field.widget.attrs.get('class', '')
-    if css_class.find(class_name) == -1:
-        css_class += " %s" % class_name
-    field.field.widget.attrs['class'] = css_class
+    if css_class:
+        if css_class.find(class_name) == -1:
+            css_class += " %s" % class_name
+    else:
+        css_class = class_name
 
+    field.field.widget.attrs['class'] = css_class
     return field
