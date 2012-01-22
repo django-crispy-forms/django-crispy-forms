14a3954146bb3a13d994cf8ac5325d6cde04f000
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2012-01-22 21:33:07 +0100

    Refactoring |with_class filter into {% crispy_field %} tag
    This way we can pass pairs of parameters to the this new tag and those will be
    added to the field attributes:
    
    We would turn our custom template into:
    
    {% crispy_field field message field.message %}
    
    And this would render:
    
    <input message='whatever the message is' ...>

--- crispy_forms/templatetags/crispy_forms_field.py
+++ crispy_forms/templatetags/crispy_forms_field.py
@@ -1,3 +1,5 @@
+from itertools import izip
+
 from django import template
 
 register = template.Library()
@@ -13,16 +15,51 @@ def is_checkbox(field):
     return field.field.widget.__class__.__name__.lower() == "checkboxinput"
 
 @register.filter
-def with_class(field):
-    class_name = field.field.widget.__class__.__name__.lower()
-    class_name = class_converter.get(class_name, class_name)
-    
-    css_class = field.field.widget.attrs.get('class', '')
-    if css_class:
-        if css_class.find(class_name) == -1:
-            css_class += " %s" % class_name
-    else:
-        css_class = class_name
+def is_password(field):
+    return field.field.widget.__class__.__name__.lower() == "passwordinput"
 
-    field.field.widget.attrs['class'] = css_class
-    return field
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s2,s3), (s4, s5), ..."
+    a = iter(iterable)
+    return izip(a, a)
+
+class CrispyFieldNode(template.Node):
+    def __init__(self, field, attrs):
+       self.field = template.Variable(field)
+       self.attrs = attrs
+
+    def render(self, context):
+        field = self.field.resolve(context)
+
+        class_name = field.field.widget.__class__.__name__.lower()
+        class_name = class_converter.get(class_name, class_name)
+        
+        css_class = field.field.widget.attrs.get('class', '')
+        if css_class:
+            if css_class.find(class_name) == -1:
+                css_class += " %s" % class_name
+        else:
+            css_class = class_name
+
+        field.field.widget.attrs['class'] = css_class
+
+        for attribute_name, attribute in self.attrs.items():
+            field.field.widget.attrs[template.Variable(attribute_name).resolve(context)] = template.Variable(attribute).resolve(context)
+
+        return field
+
+@register.tag(name="crispy_field")
+def crispy_field(parser, token):
+    """
+    {% crispy_field field attrs %}
+    """
+    token = token.split_contents()
+    field = token.pop(1)
+    attrs = {}
+
+    tag_name = token.pop(0)
+    for attribute_name, value in pairwise(token):
+        attrs[attribute_name] = value
+
+    return CrispyFieldNode(field, attrs)
