4f9c3020008abcf92080edd8628345d6a1b8cee4
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2012-05-29 21:31:55 +0200

    Avoid polluting the global template context
    The node context needs to be deepcopied from context. Updates to
    `context` variable affect the whole context of the template where `{%
    crispy %}` tag is being used.
    
    This fixes #GH-54

--- crispy_forms/templatetags/crispy_forms_tags.py
+++ crispy_forms/templatetags/crispy_forms_tags.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
+
 from django.conf import settings
 from django.forms.formsets import BaseFormSet
 from django.template import Context
@@ -16,19 +18,19 @@ TEMPLATE_PACK = getattr(settings, 'CRISPY_TEMPLATE_PACK', 'bootstrap')
 
 class ForLoopSimulator(object):
     """
-    Simulates a forloop tag, precisely:: 
-        
+    Simulates a forloop tag, precisely::
+
         {% for form in formset.forms %}
 
     If `{% crispy %}` is rendering a formset with a helper, We inject a `ForLoopSimulator` object
     in the context as `forloop` so that formset forms can do things like::
-        
+
         Fieldset("Item {{ forloop.counter }}", [...])
         HTML("{% if forloop.first %}First form text{% endif %}"
     """
     def __init__(self, formset):
         self.len_values = len(formset.forms)
-    
+
         # Shortcuts for current loop iteration number.
         self.counter = 1
         self.counter0 = 0
@@ -52,7 +54,7 @@ class ForLoopSimulator(object):
 
 
 class BasicNode(template.Node):
-    """ 
+    """
     Basic Node object that we can rely on for Node objects in normal
     template tags. I created this because most of the tags we'll be using
     will need both the form object and the helper string. This handles
@@ -67,13 +69,13 @@ class BasicNode(template.Node):
             self.helper = None
 
     def get_render(self, context):
-        """ 
+        """
         Returns a `Context` object with all the necesarry stuff for rendering the form
 
         :param context: `django.template.Context` variable holding the context for the node
 
         `self.form` and `self.helper` are resolved into real Python objects resolving them
-        from the `context`. The `actual_form` can be a form or a formset. If it's a formset 
+        from the `context`. The `actual_form` can be a form or a formset. If it's a formset
         `is_formset` is set to True. If the helper has a layout we use it, for rendering the
         form or the formset's forms.
         """
@@ -100,20 +102,21 @@ class BasicNode(template.Node):
                 raise TypeError('helper object provided to {% crispy %} tag must be a crispy.helper.FormHelper object.')
             attrs = helper.get_attributes()
 
-        # We get the response dictionary 
+        # We get the response dictionary
         is_formset = isinstance(actual_form, BaseFormSet)
         response_dict = self.get_response_dict(attrs, context, is_formset)
-        context.update(response_dict)
+        node_context = deepcopy(context)
+        node_context.update(response_dict)
 
         # If we have a helper's layout we use it, for the form or the formset's forms
         if helper and helper.layout:
             if not is_formset:
-                actual_form.form_html = helper.render_layout(actual_form, context)
+                actual_form.form_html = helper.render_layout(actual_form, node_context)
             else:
                 forloop = ForLoopSimulator(actual_form)
                 for form in actual_form.forms:
-                    context.update({'forloop': forloop})
-                    form.form_html = helper.render_layout(form, context)
+                    node_context.update({'forloop': forloop})
+                    form.form_html = helper.render_layout(form, node_context)
                     forloop.iterate()
 
         if is_formset:
@@ -126,7 +129,7 @@ class BasicNode(template.Node):
     def get_response_dict(self, attrs, context, is_formset):
         """
         Returns a dictionary with all the parameters necessary to render the form/formset in a template.
-        
+
         :param attrs: Dictionary with the helper's attributes used for rendering the form/formset
         :param context: `django.template.Context` for the node
         :param is_formset: Boolean value. If set to True, indicates we are working with a formset.
@@ -189,12 +192,12 @@ class CrispyFormNode(BasicNode):
 def do_uni_form(parser, token):
     """
     You need to pass in at least the form/formset object, and can also pass in the
-    optional `crispy_forms.helpers.FormHelper` object. 
+    optional `crispy_forms.helpers.FormHelper` object.
 
     helper (optional): A `uni_form.helper.FormHelper` object.
 
     Usage::
-    
+
         {% include crispy_tags %}
         {% crispy form form.helper %}
 
