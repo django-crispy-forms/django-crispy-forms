e72c2b2da8f4143a0a6735c2dd6608de8afc7909
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2012-11-09 12:19:41 +0100

    Fixed context copying for Django 1.2 compatibility
    Thanks to Alex Yakovlev for the patch. This closes #108

--- crispy_forms/templatetags/crispy_forms_tags.py
+++ crispy_forms/templatetags/crispy_forms_tags.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from copy import copy
+
 from django.conf import settings
 from django.forms.formsets import BaseFormSet
 from django.template import Context
@@ -51,6 +53,19 @@ class ForLoopSimulator(object):
         self.last = (self.revcounter0 == self.len_values - 1)
 
 
+def copy_context(context):
+    """
+    Copies a `Context` variable. It uses `Context.__copy__` if available
+    (introduced in Django 1.3) or copy otherwise.
+    """
+    if hasattr(context, "__copy__"):
+        return context.__copy__()
+
+    duplicate = copy(context)
+    duplicate.dicts = context.dicts[:]
+    return duplicate
+
+
 class BasicNode(template.Node):
     """
     Basic Node object that we can rely on for Node objects in normal
@@ -97,7 +112,7 @@ class BasicNode(template.Node):
         # We get the response dictionary
         is_formset = isinstance(actual_form, BaseFormSet)
         response_dict = self.get_response_dict(helper, context, is_formset)
-        node_context = context.__copy__()
+        node_context = copy_context(context)
         node_context.update(response_dict)
 
         # If we have a helper's layout we use it, for the form or the formset's forms
