f5c8c77266f6c95dbefd340b86868d35d98d7d07
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2013-05-06 21:12:09 +0200

    Removing deprecation warning for Django 1.5 and upper

--- crispy_forms/tests/urls.py
+++ crispy_forms/tests/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import *
+import django
+
+if django.get_version() >= '1.5':
+    from django.conf.urls import *
+else:
+    from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^simple/action/$', 'simpleAction', name = 'simpleAction'),
