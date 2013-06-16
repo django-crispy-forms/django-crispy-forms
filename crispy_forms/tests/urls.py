4efb084fab151909741d3273c529483bff85765e
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2013-06-16 12:50:47 +0200

    Avoiding import * in tests

--- crispy_forms/tests/urls.py
+++ crispy_forms/tests/urls.py
@@ -1,9 +1,9 @@
 import django
 
 if django.get_version() >= '1.5':
-    from django.conf.urls import *
+    from django.conf.urls import patterns, url
 else:
-    from django.conf.urls.defaults import *
+    from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
     url(r'^simple/action/$', 'simpleAction', name = 'simpleAction'),
