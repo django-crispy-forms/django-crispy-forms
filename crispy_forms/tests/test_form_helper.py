7e5b0e2d25395b30c3dce1fe12e502214e3f1c0d
John Comeau <jc@unternet.net>
2015-03-17 22:07:02 -0700

    fixed other version comparisons, although last fix raised failure count from 7 to 11

--- crispy_forms/tests/test_form_helper.py
+++ crispy_forms/tests/test_form_helper.py
@@ -23,7 +23,7 @@ from crispy_forms.helper import FormHelper, FormHelpersException
 from crispy_forms.layout import (
     Layout, Submit, Reset, Hidden, Button, MultiField, Field
 )
-from crispy_forms.utils import render_crispy_form
+from crispy_forms.utils import render_crispy_form, version_tuple
 from crispy_forms.templatetags.crispy_forms_tags import CrispyFormNode
 
 
@@ -268,7 +268,7 @@ class TestFormHelper(CrispyTestCase):
 
         settings.CRISPY_FAIL_SILENTLY = False
         # Django >= 1.4 is not wrapping exceptions in TEMPLATE_DEBUG mode
-        if settings.TEMPLATE_DEBUG and django.get_version() < '1.4':
+        if settings.TEMPLATE_DEBUG and version_tuple(django.get_version()) < version_tuple('1.4'):
             self.assertRaises(TemplateSyntaxError, lambda:template.render(c))
         else:
             self.assertRaises(TypeError, lambda:template.render(c))
@@ -361,7 +361,7 @@ class TestFormHelper(CrispyTestCase):
         self.assertEqual(html.count('<input'), 3)
         self.assertEqual(html.count('hidden'), 2)
 
-        if django.get_version() < '1.5':
+        if version_tuple(django.get_version()) < version_tuple('1.5'):
             self.assertEqual(html.count('type="hidden" name="password1"'), 1)
             self.assertEqual(html.count('type="hidden" name="password2"'), 1)
         else:
