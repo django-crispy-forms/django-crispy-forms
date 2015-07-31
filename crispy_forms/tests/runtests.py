917718387d94381a7e48d66febfa55d04784317f
Trey Hunner <trey@treyhunner.com>
2015-07-31 16:11:33 -0700

    Measure code coverage when running tests

--- crispy_forms/tests/runtests.py
+++ crispy_forms/tests/runtests.py
@@ -7,6 +7,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'crispy_forms.tests.test_settings'
 
 
 for template_pack in ('uni_form', 'bootstrap', 'bootstrap3'):
-    retval = os.system('CRISPY_TEMPLATE_PACK=%s py.test' % template_pack)
+    retval = os.system('CRISPY_TEMPLATE_PACK=%s py.test --cov=crispy_forms' % template_pack)
     if retval:
         sys.exit(1)
