def59d86b3812dd5ebdc828c3eac5f4d9310e207
Carlton Gibson <carlton.gibson@noumenal.co.uk>
2015-08-17 12:28:22 +0200

    Add twine to requirments and `setup.py` `publish`
    ... for secure uploads to PyPI.

--- setup.py
+++ setup.py
@@ -13,9 +13,11 @@ if sys.argv[-1] == 'publish':
     if os.system("pip freeze | grep wheel"):
         print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
         sys.exit()
-
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_wheel upload")
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
     print("You probably want to also tag the version now:")
     print("  git tag -a %s -m 'version %s'" % (crispy_forms.__version__, crispy_forms.__version__))
     print("  git push --tags")
