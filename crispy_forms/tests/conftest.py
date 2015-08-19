842ccb250722bfedd3d53e8b3687f21eeb621c7f
Dmitry Dygalo <d.dygalo@volsor.com>
2015-08-19 10:01:17 +0200

    Remove old base class. Add advanced_layout fixture. Complete py.test integration

--- crispy_forms/tests/conftest.py
+++ crispy_forms/tests/conftest.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 import pytest
 
+from crispy_forms.layout import Layout, Div, Field, Submit, Fieldset, HTML
+
 
 def get_skip_mark(*template_packs):
     return pytest.mark.skipif(settings.CRISPY_TEMPLATE_PACK not in template_packs,
@@ -12,3 +14,23 @@ def get_skip_mark(*template_packs):
 only_uni_form = get_skip_mark('uni_form')
 only_bootstrap = get_skip_mark('bootstrap', 'bootstrap3')
 only_bootstrap3 = get_skip_mark('bootstrap3')
+
+
+@pytest.fixture
+def advanced_layout():
+    return Layout(
+        Div(
+            Div(Div('email')),
+            Div(Field('password1')),
+            Submit("save", "save"),
+            Fieldset(
+                "legend",
+                'first_name',
+                HTML("extra text"),
+            ),
+            Layout(
+                "password2",
+            ),
+        ),
+        'last_name',
+    )
