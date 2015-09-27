a48d5a93fff22f5a1425b7371b36b16cb4e54103
David Avsajanishvili <david@davidavs.com>
2015-09-27 07:20:59 -0400

    Fix import error in Django 1.9 (BoundField)

--- crispy_forms/tests/test_tags.py
+++ crispy_forms/tests/test_tags.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.forms.forms import BoundField
+from django.forms import BoundField
 from django.forms.models import formset_factory
 from django.template import Context
 
