d32da1c9548bc5728b0b7b736f57c1b2ddb93961
Alexander Kavanaugh <kavanaugh.development@outlook.com>
2015-12-22 18:02:01 -0800

    added some tests so that coverage doesn't drop

--- crispy_forms/tests/test_utils.py
+++ crispy_forms/tests/test_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from crispy_forms.utils import list_union, list_difference, list_intersection
+from crispy_forms.utils import list_union, list_difference, list_intersection, set_hidden, render_field
 
 
 def test_list_intersection():
@@ -24,3 +24,16 @@ def test_list_set_operations():
     assert difference == ['1']
 
 
+def test_set_hidden():
+    class FakeWidget(object):
+        is_hidden = False
+
+    widget = FakeWidget()
+    set_hidden(widget)
+
+    assert widget.is_hidden is True
+
+
+def test_render_field_with_none_field():
+    rendered = render_field(field=None, form=None, form_style=None, context=None)
+    assert rendered == ''
