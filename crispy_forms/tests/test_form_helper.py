59a479724d2e81d220d74c1a1c6faba9727a89f2
Kaspars Sprogis <darklow@gmail.com>
2016-12-02 12:53:45 +0200

    Support for bootstrap4-alpha5 #658: Help text and feedback classes markup

--- crispy_forms/tests/test_form_helper.py
+++ crispy_forms/tests/test_form_helper.py
@@ -617,10 +617,12 @@ def test_error_text_inline(settings):
     html = render_crispy_form(form)
 
     help_class = 'help-inline'
+    help_tag_name = 'p'
     if settings.CRISPY_TEMPLATE_PACK == 'bootstrap3':
         help_class = 'help-block'
     elif settings.CRISPY_TEMPLATE_PACK == 'bootstrap4':
         help_class = 'form-control-feedback'
+        help_tag_name = 'div'
 
     matches = re.findall(
         '<span id="error_\d_\w*" class="%s"' % help_class, html, re.MULTILINE
@@ -639,7 +641,7 @@ def test_error_text_inline(settings):
         help_class = 'form-control-feedback'
 
     matches = re.findall(
-        '<p id="error_\d_\w*" class="%s"' % help_class,
+        '<%s id="error_\d_\w*" class="%s"' % (help_tag_name, help_class),
         html,
         re.MULTILINE
     )
@@ -688,7 +690,7 @@ def test_error_and_help_inline():
 
     # Check that help goes before error, otherwise CSS won't work
     help_position = html.find('<span id="hint_id_email" class="help-inline">')
-    error_position = html.find('<p id="error_1_id_email" class="form-control-feedback">')
+    error_position = html.find('<div id="error_1_id_email" class="form-control-feedback">')
     assert help_position < error_position
 
     # Viceversa
@@ -702,7 +704,7 @@ def test_error_and_help_inline():
 
     # Check that error goes before help, otherwise CSS won't work
     error_position = html.find('<span id="error_1_id_email" class="help-inline">')
-    help_position = html.find('<small id="hint_id_email" class="text-muted">')
+    help_position = html.find('<small id="hint_id_email" class="form-text text-muted">')
     assert error_position < help_position
 
 
