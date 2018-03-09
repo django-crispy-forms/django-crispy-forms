b6c3210a6ed4a76f3570cce362d449bb1865f0c1
Bruno Alla <browniebroke@users.noreply.github.com>
2018-03-09 13:12:56 +0000

    Fix Ambiguous IDs in checkboxes inputs (#783)
    * Add an assertion to check for non-duplicate IDs
    
    * Test case to cover the expected behaviour
    
    * Fix IDs of inputs for checkboxes select multiple

--- crispy_forms/tests/test_layout_objects.py
+++ crispy_forms/tests/test_layout_objects.py
@@ -482,3 +482,23 @@ class TestBootstrapLayoutObjects(object):
                 assert html.count('checkbox-inline"') == 3
             elif settings.CRISPY_TEMPLATE_PACK == 'bootstrap4':
                 assert html.count('form-check-inline"') == 3
+
+    def test_multiple_checkboxes_unique_ids(self):
+        test_form = CheckboxesSampleForm()
+        html = render_crispy_form(test_form)
+
+        expected_ids = [
+            'checkboxes_1',
+            'checkboxes_2',
+            'checkboxes_3',
+            'alphacheckboxes_1',
+            'alphacheckboxes_2',
+            'alphacheckboxes_3',
+            'numeric_multiple_checkboxes_1',
+            'numeric_multiple_checkboxes_2',
+            'numeric_multiple_checkboxes_3',
+        ]
+        for id_suffix in expected_ids:
+            expected_str = 'id="id_{id_suffix}"'.format(id_suffix=id_suffix)
+            assert html.count(expected_str) == 1
+
