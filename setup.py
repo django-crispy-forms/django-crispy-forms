daedf7cfe1b05caaf1e47487b8b5da7811ba02d8
Carlton Gibson <carlton.gibson@noumenal.es>
2019-05-06 22:00:11 +0200

    Add Django 2.2 to Trove classifiers.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Framework :: Django :: 1.11",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",        
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: JavaScript",
