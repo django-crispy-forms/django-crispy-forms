2207f2b9c4c2cfc646b9f28cbcdb7f39720dc4d7
David Smith <39445562+smithdc1@users.noreply.github.com>
2020-01-24 15:38:03 +0000

    Flake8 indentation (E1) (#973)

--- crispy_forms/tests/forms.py
+++ crispy_forms/tests/forms.py
@@ -128,8 +128,8 @@ class SampleFormWithMultiValueField(forms.Form):
 class CrispyEmptyChoiceTestModel(models.Model):
     fruit = models.CharField(
         choices=[
-        ('apple', 'Apple'),
-        ('pear', 'Pear')],
+            ('apple', 'Apple'),
+            ('pear', 'Pear')],
         null=True,
         blank=True,
     )
