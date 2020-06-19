a42d5343ef52b548385b408ade6c00cb191d012d
David Smith <39445562+smithdc1@users.noreply.github.com>
2020-06-19 12:22:38 +0100

    Changed urlpatterns to use path (#1040)
    url() is deprecated in Django4.1

--- crispy_forms/tests/urls.py
+++ crispy_forms/tests/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import View
 
 urlpatterns = [
-    url(r"^simple/action/$", View.as_view(), name="simpleAction"),
+    path("simple/action/", View.as_view(), name="simpleAction"),
 ]
