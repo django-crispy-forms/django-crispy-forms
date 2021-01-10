fcbddb416d31b6528bb12f9ad981a088442e8b25
Carles Pina Estany <carles@pina.cat>
2021-01-10 08:46:20 +0000

    Update copyright notices (#1106)
    Updates the year from to 2021, updates Daniel's name and add
    "contributors"

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'django-crispy-forms'
-copyright = '2009-2013, Miguel Araujo and Daniel Greenfeld'
+copyright = '2009-2021, Miguel Araujo, Daniel Feldroy and contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
