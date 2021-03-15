ed8c5180973ee53c0ac57d4e02db03ca03f5b4fc
David Smith <39445562+smithdc1@users.noreply.github.com>
2021-03-15 21:23:12 +0000

    Added html parsing tools for testing (#1128)
    * Added html parsing tools for testing
    This allows the expected html to be stored in an HTML file. Tests can then assert the layout of the whole form.

--- crispy_forms/tests/utils.py
+++ crispy_forms/tests/utils.py
@@ -1,4 +1,11 @@
+import os
+from pathlib import Path
+
 from django.test.html import Element, parse_html
+
+from crispy_forms.utils import render_crispy_form
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def contains_partial(haystack, needle, ignore_needle_children=False):
@@ -20,3 +27,14 @@ def contains_partial(haystack, needle, ignore_needle_children=False):
         for child in haystack.children
         if isinstance(child, Element)
     )
+
+
+def parse_expected(expected_file):
+    test_file = Path(TEST_DIR) / "results" / expected_file
+    with test_file.open() as f:
+        return parse_html(f.read())
+
+
+def parse_form(form):
+    html = render_crispy_form(form)
+    return parse_html(html)
