5efc847a82540d8e6979d7a54d603f52467c9f82
NoahSandler <Noahsandler@gmail.com>
2022-01-12 21:25:28 -0500

    Add modal class to bootstrap.py

--- crispy_forms/bootstrap.py
+++ crispy_forms/bootstrap.py
@@ -441,3 +441,54 @@ class UneditableField(Field):
 
 class InlineField(Field):
     template = "%s/layout/inline_field.html"
+
+
+class Modal(LayoutObject):
+    """
+    Boostrap layout object for rendering crispy forms objects inside a bootstrap modal.
+
+    The following attributes can be set:
+        - `css_id`: modal's DOM id
+        - `css_class`: modal's DOM classes
+            - NOTE: "modal" and "fade" are applied by default
+        - `title`: text to display in the modal's header
+            - NOTE: text will be wrapped in a <h5> tag
+        - `title_id`: title's DOM id
+        - `title_class`: titles's DOM classes
+            - NOTE: "modal-title" is applied by default
+
+    Example::
+
+        Modal(
+            'field1',
+            Div('field2'),
+            css_id="modal-id-ex",
+            css_class="modal-class-ex,
+            title="This is my modal"
+        )
+    """
+
+    template = "%s/layout/modal.html"
+
+    def __init__(self, *fields, **kwargs):
+        self.fields = list(fields)
+        self.template = kwargs.pop("template", self.template)
+
+        self.css_id = kwargs.pop("css_id", "modal_id")
+        self.css_class = "modal fade"
+        self.title = kwargs.pop("title", "Modal Title")
+        self.title_id = kwargs.pop("title_id", "modal_title_id")
+        self.title_class = kwargs.pop("title_class", "modal-title")
+
+        if "css_class" in kwargs:
+            self.css_class += " %s" % kwargs.pop("css_class")
+
+        kwargs = {**kwargs, "tabindex": "-1", "role": "dialog", "aria-labelledby": "%s-label" % self.css_id}
+
+        self.flat_attrs = flatatt(kwargs)
+
+    def render(self, form, form_style, context, template_pack=TEMPLATE_PACK, **kwargs):
+        fields = self.get_rendered_fields(form, form_style, context, template_pack, **kwargs)
+        template = self.get_template_name(template_pack)
+
+        return render_to_string(template, {"modal": self, "fields": fields})
