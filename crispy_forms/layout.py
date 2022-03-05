5ef6e4be58415bb0f09b977518fc782582bcb652
David Smith <39445562+smithdc1@users.noreply.github.com>
2022-03-05 16:45:03 +0000

    Improved doc string of layout.Field (#1242)

--- crispy_forms/layout.py
+++ crispy_forms/layout.py
@@ -812,8 +812,39 @@ class HTML:
 
 class Field(LayoutObject):
     """
-    Layout object, It contains one field name, and you can add attributes to it easily.
-    For setting class attributes, you need to use `css_class`, as `class` is a Python keyword.
+    A Layout object, usually containing one field name, where you can add
+    attributes to it easily.
+
+    Attributes
+    ----------
+    template : str
+        The default template which this Layout Object will be rendered
+        with.
+    attrs : dict
+        Attributes to be applied to the field. These are converted into html
+        attributes. e.g. ``data_id: 'test'`` in the attrs dict will become
+        ``data-id='test'`` on the field's ``<input>``.
+
+    Parameters
+    ----------
+    *fields : str
+        Usually a single field, but can be any number of fields, to be rendered
+        with the same attributes applied.
+    css_class : str, optional
+        CSS classes to be applied to the field. These are added to any classes
+        included in the ``attrs`` dict. By default ``None``.
+    wrapper_class: str, optional
+        CSS classes to be used when rendering the Field. This class is usually
+        applied to the ``<div>`` which wraps the Field's ``<label>`` and
+        ``<input>`` tags. By default ``None``.
+    template : str, optional
+        Overrides the default template, if provided. By default ``None``.
+    **kwargs : dict, optional
+        Additional attributes are converted into key="value", pairs. These
+        attributes are added to the ``<div>``.
+
+    Examples
+    --------
 
     Example::
 
