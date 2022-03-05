b4f015c9ee9069e6e09fe29b62aa35b6c8c5dddd
David Smith <39445562+smithdc1@users.noreply.github.com>
2022-03-05 11:03:14 +0000

    Improved `__init__` signature of `layout.Field` (#1225)

--- crispy_forms/tests/test_layout.py
+++ crispy_forms/tests/test_layout.py
@@ -766,6 +766,15 @@ def test_use_custom_control_in_uneditable_select(use_custom_control, expected_ht
     assert parse_form(form) == parse_expected(expected_html)
 
 
+def test_multiple_fields(settings):
+    "Field can accept any number of fields and apply the kwargs to all fields"
+    form = SampleForm()
+    form.helper = FormHelper()
+    form.helper.layout = Layout(Field("first_name", "last_name", css_class="form-control-lg"))
+    template_pack = settings.CRISPY_TEMPLATE_PACK
+    assert parse_form(form) == parse_expected(f"{template_pack}/test_layout/test_multiple_fields.html")
+
+
 @only_bootstrap4
 def test_fundamentals():
     """
