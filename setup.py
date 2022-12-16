c93a7e6ae59ba8c400925eb6bca8419501956c1f
David Smith <39445562+smithdc1@users.noreply.github.com>
2022-12-16 20:55:44 +0000

    Added support for Python 3.11. (#1310)

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
