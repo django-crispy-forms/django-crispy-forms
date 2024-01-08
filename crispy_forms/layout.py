ccebdbfe1105bf0f854ce0f602e06e00e0e2a4f0
Benoît Blanchon <bblanchon@users.noreply.github.com>
2024-01-08 13:11:21 +0100

    Fix documentation of `Field`'s constructor
    The documentation of `kwargs` for `Field`'s constructor was incorrect.
    It said that the attributes are added to the `<div>`, but they are actually added to the `<input>`.

--- crispy_forms/layout.py
+++ crispy_forms/layout.py
@@ -906,7 +906,7 @@ class Field(LayoutObject):
         Overrides the default template, if provided. By default ``None``.
     **kwargs : dict, optional
         Additional attributes are converted into key="value", pairs. These
-        attributes are added to the ``<div>``.
+        attributes are added to the field's ``<input>``.
 
     Examples
     --------
