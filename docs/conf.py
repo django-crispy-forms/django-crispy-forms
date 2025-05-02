dabd719113c8e7e43fa8d5e254bfa938d169e6e8
David Smith <39445562+smithdc1@users.noreply.github.com>
2025-05-02 09:17:52 +0100

    Add Project Logo. (#1415)
    * Add Project Logo.
    
    * Add logo to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -102,6 +102,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 html_theme = "sphinx_rtd_theme"
+html_logo = "_static/logo.svg"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
